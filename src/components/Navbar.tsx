59d6f969b2a6c1428c64087f7df1c8231a8d1c43
Anoop Kumar A <92391766+AnoopkumarA@users.noreply.github.com>
2024-11-11 01:49:07 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -195,7 +195,7 @@ const Navbar = () => {
             <motion.button
               whileTap={{ scale: 0.95 }}
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="md:hidden relative p-2 text-cyber-blue hover:bg-cyber-blue/10 rounded-lg group -left-8 md:-left-0"
+              className="md:hidden relative p-2 text-cyber-blue hover:bg-cyber-blue/10 rounded-lg group -left-6 md:-left-0"
             >
               <div className="relative z-10">
                 {isMobileMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
